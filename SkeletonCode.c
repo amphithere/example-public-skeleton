2f828fc73cb3be51782fa9838b74f66e265b528f
Lisa Munin <lisa.munin@formidable.com>
2021-03-25 12:22:13 -0700

    added sample skeleton code example with comments

---
+++ SkeletonCode.c
@@ -0,0 +1,25 @@
+/**
+ * SampleCode.c
+ * WhateverCourseNameHere 12345
+ */
+
+int sampleSkeletonCode();
+void searchForThisExactHeaderName();
+
+int main()
+{
+  sampleSkeletonCode();
+}
+
+sampleSkeletonCode()
+{
+  // This is an example comment that could be used to indicate
+  // what needs to be done in this skeleton function
+  return 0;
+}
+
+void searchForThisExactHeaderName()
+{
+  // searching for this exact header will return repositories
+  // with this code snippet
+}
